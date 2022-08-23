cf6e7255b6df884e674b583ee13dba812bbef4ab
Artur Żarek <34176570+ajz3d@users.noreply.github.com>
2022-08-23 19:25:34 +0200

    Update bl_info

--- scripts/python/__init__.py
+++ scripts/python/__init__.py
@@ -1,15 +1,14 @@
 # coding=utf-8
 bl_info = {
     "name": "Houdini-Blender Clipboard",
-    "description": "Enables quick geometry interchange between Houdini and Blender.",
+    "description": "Enables quick geometry exchange between Houdini and Blender.",
     "author": "Artur J. Żarek (ajz3d)",
     "version": (1, 0),
     "blender": (3, 2, 2),
     "location": "File > Import-Export",
     "warning": "", # used for warning icon and text in addons panel
-    "doc_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
-                "Scripts/My_Script",
-    "tracker_url": "https://developer.blender.org/maniphest/task/edit/form/2/",
+    "doc_url": "https://github.com/ajz3d/houdini-blender-clipboard",
+    "tracker_url": "https://github.com/ajz3d/houdini-blender-clipboard/issues",
     "support": "COMMUNITY",
     "category": "Import-Export",
 }
