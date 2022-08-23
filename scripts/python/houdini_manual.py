f412062cd14d687026a5f22498f02fa62e7999b8
Artur Żarek <34176570+ajz3d@users.noreply.github.com>
2022-08-23 13:18:08 +0200

    Change licence from GPL3 to GPL3 or later

--- scripts/python/houdini_manual.py
+++ scripts/python/houdini_manual.py
@@ -4,7 +4,7 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# any later version.
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -50,6 +50,8 @@ def houdini_export():
 
     # Configure and export selected SOPs.
     for sop in sops:
+        if type(sop) == hou.RopNode:
+            continue
         abc_rop = sop.parent().createNode('rop_alembic')
         abc_rop.setFirstInput(sop)
 
